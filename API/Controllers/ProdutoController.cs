aabdaac7cc8999dbd2abb6b89bbdee9df34210f6
apolozs <70415486+apolozs@users.noreply.github.com>
2021-08-25 22:39:36 -0300

    Criação DataContext e listagem de itens

--- API/Controllers/ProdutoController.cs
+++ API/Controllers/ProdutoController.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+using API.Data;
 using API.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -5,17 +8,29 @@ namespace API.Controllers
 {
     [ApiController]
     [Route("api/produto")]
-
+    
     public class ProdutoController : ControllerBase
     {
+        
+        private readonly DataContext _context;
+
+        //Contrutor
+        public ProdutoController(DataContext context) =>   _context = context;
+        
         //POST: /api/produto/create
         [HttpPost]
         [Route("create")]
         public Produto Create(Produto produto)
         {
-            produto.Nome += " Alterado";
+
+            _context.Produtos.Add(produto);
+           _context.SaveChanges();
             return produto;
-        }
-         
+        }        
+
+        //GET: /api/produto/create
+        [HttpGet]
+        [Route("list")]
+        public List<Produto> list() => _context.Produtos.ToList();
     }
 }
--- API/Models/Produto.cs
+++ API/Models/Produto.cs
@@ -1,10 +1,23 @@
+using System;
 namespace API.Models
 {
     public class Produto
     {
+        public Produto() => CriadoEm = DateTime.Now ;
+        
 
+        //Atributos ou propiedades
+        public int id { get; set;}
         public string Nome { get; set;}
-
         public double Preco { get; set;}
+        public string Descrição { get; set; }
+        public int Quantidade { get; set; }
+        public DateTime CriadoEm { get; set; }
+
+        //Criando o ToString
+        public override string ToString() =>
+        
+            $"Nome: {Nome} | Preço: {Preco:C2} | CriadoEm em: {CriadoEm}";
+    
     }
 }
