9d5eb88458f992da1dd4e889cbd4c4a105badbab
apolozs <70415486+apolozs@users.noreply.github.com>
2021-09-01 22:10:40 -0300

    Api com Filtro by ID

--- API/Controllers/ProdutoController.cs
+++ API/Controllers/ProdutoController.cs
@@ -8,15 +8,15 @@ namespace API.Controllers
 {
     [ApiController]
     [Route("api/produto")]
-    
+
     public class ProdutoController : ControllerBase
     {
-        
+
         private readonly DataContext _context;
 
         //Contrutor
-        public ProdutoController(DataContext context) =>   _context = context;
-        
+        public ProdutoController(DataContext context) => _context = context;
+
         //POST: /api/produto/create
         [HttpPost]
         [Route("create")]
@@ -24,13 +24,29 @@ namespace API.Controllers
         {
 
             _context.Produtos.Add(produto);
-           _context.SaveChanges();
+            _context.SaveChanges();
             return produto;
-        }        
+        }
 
-        //GET: /api/produto/create
+        //GET: /api/produto/list
         [HttpGet]
         [Route("list")]
         public List<Produto> list() => _context.Produtos.ToList();
+
+
+        //get /api/produto/getid/id
+
+        [HttpGet]
+        [Route("getid/{id}")]
+        //public Produto getid(int id) => _context.Produtos.Find(id);
+        public IActionResult getid([FromRoute] int id)
+        {
+            Produto produto = _context.Produtos.Find(id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            return Ok(produto);
+        }
     }
 }
